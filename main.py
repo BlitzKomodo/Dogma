b4bcf2c0e166d420dcedbce8434b0edbf157d2c3
Kerb <hakkerby01@gmail.com>
2024-03-20 06:53:22 -0700

    💵 fixed issue #11 Made a USD to riels converter Made a Riels to USD converter

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ def main(N):
 
 n = 3000
 
-print(main(n))
+def usd_to_riels(x):
+    return f"\n[i] \t{x} \tUSD \t= \t{4053 * x} Riels\n"
+
+def riels_to_usd(x):
+    return f"\n[i] \t{x} \tRiels \t= \t{x / 4053} USD\n"
+
+
+print(usd_to_riels(1))
+print(riels_to_usd(10000))
